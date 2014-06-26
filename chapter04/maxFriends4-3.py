9f48c1000dfb46239bab2ad2b9890f1ad9c59d0f
Vages <eirikvageskar@gmail.com>
2014-06-26 17:29:43 +0200

    Fix tab widths to suit GitHub web view

--- chapter04/maxFriends4-3.py
+++ chapter04/maxFriends4-3.py
@@ -5,17 +5,17 @@ def maxParty(G, K):
 	
 	for u in G:
 		for v in G[u]:
-			count[u] += K-1				# Count the number of friends each node has
+			count[u] += K-1		# Count the number of friends each node has
 
 	Q = [u for u in G if count[u] < K]	# A dictionary of all nodes that already have less than K friends
-	S = [u for u in G] 					# A copy of all nodes in the original G dictionary; the solution set
+	S = [u for u in G] 			# A copy of all nodes in the original G dictionary; the solution set
 	
-	while Q:							# As long as there are unprocessed nodes with too few friends
-		u = Q.pop()						# Take a node that has too few friends 
-		S.remove(u)						# Remove it from the solution set
-		for v in G[u]:					# For each friend v, decrease their friend count, now that u is gone.
+	while Q:				# As long as there are unprocessed nodes with too few friends
+		u = Q.pop()			# Take a node that has too few friends 
+		S.remove(u)			# Remove it from the solution set
+		for v in G[u]:			# For each friend v, decrease their friend count, now that u is gone.
 			count[v] -= 1
-			if count[v] == K-1:			# Should a node reach a count below K, add this to Q
+			if count[v] == K-1:	# Should a node reach a count below K, add this to Q
 				Q.append(v)
 
 	return S
